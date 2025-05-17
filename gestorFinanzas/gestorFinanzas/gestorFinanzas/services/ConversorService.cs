494cf3cb3932916c4536e45ca84fa5e07247cacd
SebastianCely <juanescely@hotmail.com>
2025-05-17 07:36:40 -0500

    Se agregan los controladores, servicios y modelos para la conversión.

---
+++ gestorFinanzas/gestorFinanzas/gestorFinanzas/services/ConversorService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using gestorFinanzas.models;
+
+namespace gestorFinanzas.services
+{
+    public class ConversorService
+    {
+        public static Conversor? convertirMoneda(string monedaBase, string monedaDestino, double monto)
+        {
+            Conversor? conversor = new Conversor();
+
+            string url = $"https://v6.exchangerate-api.com/v6/04a67607a37a70cc5466f131/pair/{monedaBase}/{monedaDestino}/{monto}";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+
+                    HttpResponseMessage respuesta = client.GetAsync(url).GetAwaiter().GetResult();
+
+                    respuesta.EnsureSuccessStatusCode();
+
+                    string solicitud = respuesta.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                    conversor = JsonSerializer.Deserialize<Conversor>(solicitud);
+
+                    return conversor;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return null;
+            }
+        }
+    }
+}
